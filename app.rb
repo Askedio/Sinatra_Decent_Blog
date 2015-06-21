064c41b469139cdcaa2e3b8e244b8e99deda6663
william bowman <gcphost@gmail.com>
2015-06-20 21:37:36 -0500

    Example using DataMapper

--- app.rb
+++ app.rb
@@ -1,7 +1,31 @@
+require 'rubygems'
 require 'sinatra'
+require 'dm-core'
+require 'dm-sqlite-adapter'
+require 'dm-timestamps'
+require 'dm-validations'
+require 'dm-migrations'
+DataMapper.setup( :default, "sqlite3://#{Dir.pwd}/my_app.db" )
 
-class HelloWorldApp < Sinatra::Base
-  get '/' do
-    "Hello, world!"
-  end
+class Post
+    include DataMapper::Resource
+ 
+    property :slug       , String   , key: true, unique_index: true, default: lambda { |resource,prop| resource.title.downcase.gsub " ", "-" }
+    property :title      , String   , required: true
+    property :body       , Text     , required: true
+    property :created_at , DateTime
+    property :updated_at , DateTime
+end
+ 
+Post.auto_upgrade!
+
+get '/create' do
+  Post.create title: "new post 2", slug: "test2", body: "test"
+  Post.create title: "new post 3", slug: "test3", body: "test"
+end
+
+
+get '/' do
+  @posts = Post.all
+  erb :index 
 end
--- install.bat
+++ install.bat
@@ -1,1 +1,8 @@
 gem install sinatra
+gem install sqlite3-ruby
+gem install dm-core
+gem install do_sqlite3
+gem install dm-sqlite-adapter
+gem install dm-timestamps
+gem install dm-validations
+gem install dm-migrations
