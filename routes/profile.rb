9b73c7a5bc142c280ac9bc1dd05a31b01b8c3cea
william bowman <gcphost@gmail.com>
2015-06-22 00:08:39 -0500

    Editable profiles

--- routes/profile.rb
+++ routes/profile.rb
@@ -4,11 +4,54 @@
       module Profile
 		def self.registered(app)
 
+			  app.get '/profile/create' do 
+				protected!
+				@persons = Person.all
+				erb :"admin/profile/create" 
+			  end
+
+			  app.post '/profile/create' do
+				protected!
+
+
+				person = Person.new(:name => params[:name], :avatar => params[:avatar], :about => params[:about], :password => params[:password])
+
+				if person.save
+				  redirect '/'
+				else
+				  person.errors.each do |e|
+				   puts e
+				  end
+				  halt 500
+				end
+			  end
+
+			  app.get '/profile/:id' do 
+				protected!
+				@persons = Person.all
+				@person ||= Person.first(:name => params[:id]) || halt(404)
+				erb :"admin/profile/create" 
+			  end
+
+			  app.post '/profile/:id' do
+				protected!
+				person ||= Person.first(:name => params[:id]) || halt(404)
+				person.update(:name => params[:name], :avatar => params[:avatar], :about => params[:about], :password => params[:password])
+			    redirect '/profile'
+			  end
+
+			  app.get '/profile/delete/:id' do
+				protected!
+				person ||= Person.first(:name => params[:id]) || halt(404)
+				halt 500 unless person.destroy
+				redirect '/'
+			  end
+			  
 			  app.get '/profile' do 
 				protected!
 				@persons = Person.all
 				@person ||= Person.first(:name => session[:username]) || halt(404)
-				erb :"admin/profile/edit" 
+				erb :"admin/profile/create" 
 			  end
 
 			  app.post '/profile' do
@@ -17,6 +60,7 @@
 				person.update(:name => params[:name], :avatar => params[:avatar], :about => params[:about], :password => params[:password])
 			    redirect '/profile'
 			  end
+
 
 		  end
 	   end
