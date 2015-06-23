cf8f179f7974de0b06889842e6fe23f59acaa7dc
william bowman <gcphost@gmail.com>
2015-06-23 11:17:52 -0500

    Clear cloudinary.rb for public repo (need local settings...)

--- config/cloudinary.rb
+++ config/cloudinary.rb
@@ -1,5 +1,5 @@
 Cloudinary.config do |config|
-  config.cloud_name = "asked-io"
-  config.api_key = "225663616768815"
-  config.api_secret = "Xz0rlrMJcZfKn9Rb-9FjgwG7k4U"
+  config.cloud_name = ""
+  config.api_key = ""
+  config.api_secret = ""
 end
