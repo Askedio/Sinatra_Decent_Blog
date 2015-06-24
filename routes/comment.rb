d0e15d68897990aac3178a470faee4596291ed8d
william bowman <gcphost@gmail.com>
2015-06-23 23:20:50 -0500

    Added mailling list signup Fixed honeypot on comments

--- routes/comment.rb
+++ routes/comment.rb
@@ -5,7 +5,7 @@
 		def self.registered(app)
 
 			  app.post '/comment/:id' do
-				#halt 404 unless params[:website].nil?
+				halt 404 unless params[:website].empty?
 				post ||= Post.get(params[:id]) || halt(404)
 				post.comments.new(:body => params[:body], :name => params[:name], :email => params[:email])
 				  if !post.save
