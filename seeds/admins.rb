3d4b4f9f853da56798698a4b3cb45ffd84bacd87
william bowman <gcphost@gmail.com>
2015-06-29 11:55:15 -0500

    Updated seed to add role/permissions

--- seeds/admins.rb
+++
@@ -1,4 +0,0 @@
-if Person.count == 0
-  person = Person.create(:name => 'Admin', :slug => 'admin', :password => 'password', :about => 'Default admin account, edit profile then change name and password.', :avatar => '/images/avatar.png')
-
-end
---
+++ seeds/default.rb
@@ -0,0 +1,17 @@
+if Permission.count == 0
+  Permission.create(:title => 'roles')
+  Permission.create(:title => 'permissions')
+end
+
+if Role.count == 0
+  Role.create(:title => 'Admin', :permissions => Permission.all)
+end
+
+if Person.count == 0
+  Person.create(:name => 'Admin',
+          :slug => 'admin',
+          :password => 'password',
+          :about => 'Default admin account, edit profile then change name and password.',
+          :avatar => '/images/avatar.png',
+          :roles => Role.all)
+end
